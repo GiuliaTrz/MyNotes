0f9984f7769513b102050dc972360d12bfd0ffc1
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-07-18 23:05:39 +0200

    Added demo javascript functions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-alert ("Hello world!");
+/*
+    App Elements
+*/
+const btnAddNote = document.getElementById("btnAddNote");
+const notesBox = document.getElementById("notesBox");
+const btnExportAll = document.getElementById("btnExportAll");
+
+// Converts the date object to a specific date format 
+function formatDate(date){
+    return date.toLocaleDateString();
+}
+
+/*
+    BUTTON EVENTS
+*/
+function onbtnAddNoteClicked(){
+    alert("New note button clicked!");
+}
+
+function onbtnExportAllClicked(){
+    alert("Export all button clicked!");
+}
+
+
+
+// Attach events
+btnAddNote.onclick = onbtnAddNoteClicked;
+btnExportAll.onclick = onbtnExportAllClicked;
+
