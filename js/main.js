67f685bdb2c984db1ca988f18034517fd6d0f4b1
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-07-19 22:15:17 +0200

    Basic Javascript for the editor

--- js/main.js
+++ js/main.js
@@ -5,6 +5,16 @@ const btnAddNote = document.getElementById("btnAddNote");
 const notesBox = document.getElementById("notesBox");
 const btnExportAll = document.getElementById("btnExportAll");
 
+/*
+    Editor controls
+*/
+const btnHome = document.getElementById("btnHome");
+const btnNewNote = document.getElementById("btnNewNote");
+const btnDelete  = document.getElementById("btnDelete");
+const btnExport  = document.getElementById("btnExport");
+const txtNoteTitle = document.getElementById("txtNoteTitle");
+const txtNoteBody  = document.getElementById("txtNoteBody");
+
 // Converts the date object to a specific date format 
 function formatDate(date){
     return date.toLocaleDateString();
@@ -21,9 +31,31 @@ function onbtnExportAllClicked(){
     alert("Export all button clicked!");
 }
 
+// - Editor buttons
 
+function onbtnHomeClicked(){
+    alert("Home!");
+}
+
+function onbtnNewNoteClicked(){
+    alert("New note!");
+}
+
+function onbtnDeleteClicked(){
+    alert("Note deleted!");
+}
+
+function onbtnExportClicked(){
+    console.info("Note title: " + txtNoteTitle.value);
+    console.info("Note body: " + txtNoteBody.value);
+}
 
 // Attach events
 btnAddNote.onclick = onbtnAddNoteClicked;
 btnExportAll.onclick = onbtnExportAllClicked;
 
+// - Editor buttons
+btnNewNote.onclick = onbtnNewNoteClicked;
+btnDelete.onclick =  onbtnDeleteClicked;
+btnExport.onclick = onbtnExportClicked;
+btnHome.onclick = onbtnHomeClicked;
