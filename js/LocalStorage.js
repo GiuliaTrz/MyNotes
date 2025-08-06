5ae32cbfb064b81c6eea28d270cd37bcb220195a
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-08-06 23:25:13 +0200

    LocalStorage basic CRUD operations

--- js/LocalStorage.js
+++ js/LocalStorage.js
@@ -63,4 +63,81 @@ class LocalStorageDB {
         db.data[key]= value;
         this._saveDB(db);
     }
+
+    /**
+     * Updates an existing object in the database
+     * @param {string} key the key used to store the object 
+     * @param {*} newValue the value we want to archive
+     * @returns {boolean} True if the update was successful, false if the key is not already in the database 
+     */
+    update(key, newValue){
+        const db = this._getDB();
+        if(db.data[key]){ // if the key exists
+            db.data[key] = newValue;
+            this._saveDB(db);
+            return true;
+        }
+        return false; // key not found
+    }
+
+    /**
+     * Saves an object to the database by updating it if it already exists or creating it if it doesn't.
+     * 
+     * @param {string} key the key used to store the object  
+     * @param {*} value the value we want to archive
+     * @returns {boolean} True if the object was created (didn't exist), false it was updated
+     */
+    save(key,value){
+        const db = this._getDB();
+        const exists = key in db.data;
+        db.data[key]= value;
+        this._saveDB(db);
+        return !exists;
+    }
+
+    /**
+     * Removes an object from the database.
+     * @param {string} key the key used to store the object  
+     * @returns {boolean} True if the delete was successful, false if the key is not already in the database 
+     */
+    delete(key){
+        const db = this._getDB();
+        if(db.data[key]){ // if the key exists
+            delete db.data[key]
+            this._saveDB(db);
+            return true;
+        }
+        return false; // key not found
+    }
+
+    /**
+     * Retrieves an object from the database.
+     * @param {string} key the key used to store the object 
+     * @returns {*} The stored value or null if not found
+     */
+    get(key){
+        const db = this._getDB();
+        if(db.data[key]){ // if the key exists
+            return db.data[key]
+        }
+        return null; // key not found
+    }
+
+
+    /**
+     * Retrieves all objects in the database as a key-value object
+     * @returns {object} An object containing all key-value pairs.
+     */
+    getAll(){
+        const db = this._getDB();
+        return db.data;
+    }
+    
+    /**
+     * Serializes the entire database including metadata.
+     * @returns {string} A JSON string representation of the entire database
+     */
+    serialize(){
+        return JSON.stringify(this._getDB());
+    }
 }
