cff6fd0961c12d542cb79dd5b3d34540d3fe4511
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-08-06 22:54:54 +0200

    Implemented _getDB(), _saveDB(db)

--- js/LocalStorage.js
+++ js/LocalStorage.js
@@ -17,12 +17,39 @@ class LocalStorageDB {
         }
     }
 
+    /**
+     * Initializes a new database structure in LocalStorage
+     * @private
+     */
     _initDB() {
         const db = {
             name: this.dbName,
             createdAt: new Date().toISOString(),
             data: {},
         };
+        localStorage.setItem(this.dbName, JSON.stringify(db));
+    }
+
+    /**
+     * Retrieves the entire database object
+     * @private
+     * @returns The parsed database object or null if not found
+     */
+    _getDB(){
+        const db = localStorage.getItem(this.dbName);
+        if(db){ 
+            return JSON.parse(db);
+        }else{ // database not found!
+            return null;
+        }
+    }
+
+    /**
+     * Saves the entire database object to LocalStorage
+     * @private
+     * @param {*} db - The database object to save
+     */
+    _saveDB(db){
         localStorage.setItem(this.dbName, JSON.stringify(db));
     }
 }
