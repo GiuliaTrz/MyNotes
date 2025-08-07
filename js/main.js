ede33c8ecae4d766fd31ddeb36a801c6ba9bb27f
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-08-07 21:42:55 +0200

    Added addNoteToList

--- js/main.js
+++ js/main.js
@@ -16,17 +16,40 @@ const btnNewNote = document.getElementById("btnNewNote");
 const btnDelete  = document.getElementById("btnDelete");
 const btnExport  = document.getElementById("btnExport");
 const txtNoteTitle = document.getElementById("txtNoteTitle");
-const txtNoteBody  = document.getElementById("txtNoteBody");
+const txtnoteDate  = document.getElementById("txtnoteDate");
 
 const allNotes = storage.getAll();
 
 Object.keys(allNotes).forEach(key => {
-  addNoteToList(allNotes[key]);
+  addNoteToList(key, allNotes[key]);
 });
 
+/**
+ * Method called for each note in the database.
+ * 
+ * Adds the note to the notesBox div
+ * @param {*} key The note UUID
+ * @param {*} note The Note Object
+ */
+function addNoteToList(key, note) {
+  const noteDiv = document.createElement("div");
+  noteDiv.setAttribute("id",key);
+  noteDiv.setAttribute("class","d-flex justify-content-between py-2 note-item");
 
-function addNoteToList(note) {
-  alert(JSON.stringify(note));
+  const noteTitle = document.createElement("span");
+  const noteDate = document.createElement("span");
+
+  noteTitle.setAttribute("class","spnNoteTitle");
+  noteDate.setAttribute("class","spnNoteDate");
+  
+  noteTitle.innerText = note.title;
+  noteDate.innerText = note.timestamp;
+
+  noteDiv.appendChild(noteTitle);
+  noteDiv.appendChild(noteDate);
+
+  const notesBox = document.getElementById("notesBox");
+  notesBox.appendChild(noteDiv);
 }
 
 // Converts the date object to a specific date format 
@@ -57,7 +80,7 @@ function onbtnHomeClicked(){
 }
 
 function onbtnNewNoteClicked(){
-    var note = new Note(txtNoteTitle.value, txtNoteBody.value);
+    var note = new Note(txtNoteTitle.value, txtnoteDate.value);
     let uuid = self.crypto.randomUUID();
     storage.save(uuid, note);
 }
@@ -68,7 +91,7 @@ function onbtnDeleteClicked(){
 
 function onbtnExportClicked(){
     console.info("Note title: " + txtNoteTitle.value);
-    console.info("Note body: " + txtNoteBody.value);
+    console.info("Note body: " + txtnoteDate.value);
 }
 
 // Attach events
